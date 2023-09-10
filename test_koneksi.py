d7d011f36d82c019b53bcedca54418d746ddccd2
Fathul <61180722+basyair7@users.noreply.github.com>
2023-09-10 11:02:37 +0700

    Update test_koneksi.py

--- test_koneksi.py
+++ test_koneksi.py
@@ -2,8 +2,8 @@ import mysql.connector as mysql
 
 #******** Setting akun Database ********#
 host_db = 'localhost'
-user_db = 'basyair7'
-passwd_db = '0712'
+user_db = ''
+passwd_db = ''
 
 #**************************************#
 
